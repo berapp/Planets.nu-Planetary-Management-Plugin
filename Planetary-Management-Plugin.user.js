5cde5aac9e07e43b307ab58b26a372a5eff07ee8
berapp <bert.rapp@gmail.com>
2026-07-24 14:17:36 -0400

    Bump version to 2026.7.14

--- Planetary-Management-Plugin.user.js
+++ Planetary-Management-Plugin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Planets.nu - Planetary Management Plugin
 // @description   Planetary Management Plugin
-// @version       2026.7.13
+// @version       2026.7.14
 // @copyright	  2014, Dotman, Forked
 // @license		  CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
 // @author        Dotma
